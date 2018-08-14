4e93355d80582dd491600522a4fa4c42e5606559
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2018-08-14 14:40:30 +1000

    Add password check program.

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,12 @@
 """
-Jesse Kokanovic
+Checks password for minimum characters.
 """
+
+MIN_LENGTH = 3
+
+password = input("Choose password: ")
+length = len(password)
+while length < MIN_LENGTH:
+    password = input("Password must be longer than {} characters: ".format(MIN_LENGTH))
+    length = len(password)
+print("*" * length)
